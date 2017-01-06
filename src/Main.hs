ab94fcb71727de400fc9e73195fa82b5b49ec9ad
klangner <klangner@gmail.com>
2017-01-06 16:07:04 +0100

    area

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,7 @@
 module Main where
 
+import Prelude
+
+
+main :: IO ()
+main = putStrLn "Hello, world!"
--- src/Shape.hs
+++ src/Shape.hs
@@ -1,19 +1,60 @@
-module Shape where
+module Shape ( Radius
+             , Shape
+             , Side
+             , Vertex
+             , area
+             , circle
+             , rectangle
+             , regularPolygon
+             , rtTriangle
+             , square
+             )where
 
 type Radius = Float
 type Side = Float
 type Vertex = (Float, Float)
 
+-- | Basic shapes
 data Shape = Rectangle Side Side
            | Ellipse Radius Radius
            | RtTriangle Side Side
            | Polygon [Vertex]
            deriving (Show)
 
+
+-- | Unit square constructor
 square :: Shape
 square = Rectangle 1 1
 
-regularPolygon :: Int -> Side -> Shape
+
+-- | Regular polygon constructor
+regularPolygon :: Int    -- ^ Number of vertices
+               -> Side   -- ^ Side length
+               -> Shape
 regularPolygon n s = Polygon (map (\p -> (cos p, sin p)) ps)
     where ps = [2*pi*(fromIntegral k)/(fromIntegral n) | k <- [0..(n - 1)]]
 
+
+-- | Circle constructor
+circle :: Radius -> Shape
+circle r = Ellipse r r
+
+
+-- | Right triangle constructor
+rtTriangle :: Side -> Shape
+rtTriangle  s = RtTriangle s s
+
+
+-- | Rectangle constructor
+rectangle :: Side       -- ^ Width
+          -> Side       -- ^ Height
+          -> Shape
+rectangle w h = Rectangle w h
+
+
+-- | Calculare shape area
+area :: Shapre -> Float
+area (Rectangle w h) = w*h
+area (Ellipse r1 r2) = 1
+area (RtTrangle s1 s2) = s1*s2/2
+area (Polygon vs) = 1
